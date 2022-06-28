bb600fb6ecd0fabe00f348532a0cd301df535ad2
Minseo <passxy@naver.com>
2022-06-28 14:49:49 +0900

    안녕

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(function(){
+   //전체메뉴보기 클릭
+   $('.btn_show_all_menu').click(function(){
+        $('.all_menu').fadeIn(300);
+   });
+   //전체메뉴닫기 클릭 
+   $('.btn_hide_all_menu').click(function(){
+    $('.all_menu').fadeOut(300);
+    });
+});
